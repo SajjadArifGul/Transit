732710470f398800c4aea4a1d73665be55b18c57
Sajjad Office <sajjad@office.com>
2017-02-06 15:31:07 +0500

    Working on Registeration

--- Transit.Web/App_Start/BundleConfig.cs
+++ Transit.Web/App_Start/BundleConfig.cs
@@ -8,8 +8,27 @@ namespace Transit.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +45,33 @@ namespace Transit.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Theme").Include(
+                      "~/Theme/vendors/bower_components/morris.js/morris.css",
+                      "~/Theme/vendors/bower_components/datatables/media/css/jquery.dataTables.min.css",
+                      "~/Theme/vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.css",
+                      "~/Theme/dist/css/style.css"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/Theme").Include(
+                      "~/Theme/vendors/bower_components/jquery/dist/jquery.min.js",
+                      "~/Theme/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js",
+                      "~/Theme/vendors/bower_components/datatables/media/js/jquery.dataTables.min.js",
+                      "~/Theme/dist/js/jquery.slimscroll.js",
+                      "~/Theme/vendors/bower_components/moment/min/moment.min.js",
+                      "~/Theme/vendors/bower_components/simpleWeather/jquery.simpleWeather.min.js",
+                      "~/Theme/dist/js/simpleweather-data.js",
+                      "~/Theme/vendors/bower_components/waypoints/lib/jquery.waypoints.min.js",
+                      "~/Theme/vendors/bower_components/Counter-Up/jquery.counterup.min.js",
+                      "~/Theme/dist/js/dropdown-bootstrap-extended.js",
+                      "~/Theme/vendors/jquery.sparkline/dist/jquery.sparkline.min.js",
+                      "~/Theme/vendors/chart.js/Chart.min.js",
+                      "~/Theme/vendors/bower_components/raphael/raphael.min.js",
+                      "~/Theme/vendors/bower_components/morris.js/morris.min.js",
+                      "~/Theme/dist/js/morris-data.js",
+                      "~/Theme/vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.js",
+                      "~/Theme/dist/js/init.js",
+                      "~/Theme/dist/js/dashboard-data.js"));
         }
     }
 }
