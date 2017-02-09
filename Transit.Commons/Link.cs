e3ca5d0b544923a15bbaae33fc33d6e8bd7fe48a
Sajjad Office <sajjadarifgul@gmail.com>
2017-02-09 15:57:54 +0500

    Link Common Class Added

---
+++ Transit.Commons/Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transit.Commons
+{
+    public class Link
+    {
+        public string Text { get; set; }
+        public string URL { get; set; }
+        public string Title { get; set; }
+        public string Target { get; set; }
+        public string Rel { get; set; }
+    }
+}
