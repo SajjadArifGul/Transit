2292d139f309601b13ed2e8b41da70ee074b628c
Sajjad Office <sajjadarifgul@gmail.com>
2017-02-09 13:29:04 +0500

    Added Entity Framework and DataContext in Data Layer.

---
+++ Transit.Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Transit.Entities;
+
+namespace Transit.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<BusStop> BusStops { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+    }
+}
