5b424f48cac31431717e6df4ba69e46162a4f858
Sajjad Office <sajjadarifgul@gmail.com>
2017-02-09 15:12:37 +0500

    Roles Added, Default Admin Added, Login RememberMe Code done.

--- Transit.Web/Controllers/AccountController.cs
+++ Transit.Web/Controllers/AccountController.cs
@@ -153,15 +153,20 @@ namespace Transit.Web.Controllers
             {
                 var user = new ApplicationUser { Name=model.Name, UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
+
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    ////Assign Default Seller Role to user Here      
+                    //await this.UserManager.AddToRoleAsync(user.Id, "User");
+                    ////Ends Here
 
                     return RedirectToAction("Index", "Home");
                 }
--- Transit.Web/Startup.cs
+++ Transit.Web/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using Transit.Web.Models;
 
 [assembly: OwinStartupAttribute(typeof(Transit.Web.Startup))]
 namespace Transit.Web
@@ -9,6 +12,45 @@ namespace Transit.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var AdminRole = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                AdminRole.Name = "Admin";
+                roleManager.Create(AdminRole);
+
+                var user = new ApplicationUser();
+                user.Name = "Sajjad Arif Gul";
+                user.UserName = "sajjadarifgul@gmail.com";
+                user.Email = "sajjadarifgul@gmail.com";
+                string userPWD = "Sajjad12345;"; //this password is useless. Dont try to hack. -gul
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            // Creating Manager role    
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var ManagerRole = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                ManagerRole.Name = "Manager";
+                roleManager.Create(ManagerRole);
+            }
         }
     }
 }
