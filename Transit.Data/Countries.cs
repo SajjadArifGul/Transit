64f5ddd38625b25279ce798eefee1d292667a3a4
Sajjad Office <sajjadarifgul@gmail.com>
2017-02-27 11:22:41 +0500

    Countries Data and Service

---
+++ Transit.Data/Countries.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transit.Entities;
+
+namespace Transit.Data
+{
+    public class Countries
+    {
+        DataContext db = new DataContext();
+
+        public List<Country> GetAll()
+        {
+            return db.Countries.ToList();
+        }
+
+        public bool Add(Country country)
+        {
+            try
+            {
+                db.Countries.Add(country);
+
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(Country country)
+        {
+            try
+            {
+                Country countryInDB = db.Countries.Where(t => t.ID == country.ID).FirstOrDefault();
+
+                if (countryInDB != null)
+                {
+                    countryInDB.Name = country.Name;
+                    countryInDB.IsActive = country.IsActive;
+                    countryInDB.ModifiedBy = country.ModifiedBy;
+                    countryInDB.ModifiedOn = country.ModifiedOn;
+
+                    db.Entry(countryInDB).State = System.Data.Entity.EntityState.Modified;  //should be updated.
+
+                    db.SaveChanges();
+                    return true;
+                }
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Country country)
+        {
+            try
+            {
+                Country countryInDB = db.Countries.Where(t => t.ID == country.ID).FirstOrDefault();
+
+                if (countryInDB != null)
+                {
+                    db.Entry(countryInDB).State = System.Data.Entity.EntityState.Deleted;
+
+                    db.SaveChanges();
+                    return true;
+                }
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Transit.Services/CountriesService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transit.Data;
+using Transit.Entities;
+
+namespace Transit.Services
+{
+    public class CountriesService
+    {
+        Countries countries = new Countries();
+
+        #region Define as Singleton
+        private static CountriesService _Instance;
+
+        public static CountriesService Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new CountriesService();
+                }
+
+                return (_Instance);
+            }
+        }
+
+        private CountriesService()
+        {
+        }
+        #endregion
+
+        public List<Country> GetAllCountries()
+        {
+            return countries.GetAll();
+        }
+
+        public Country GetCountryByID(int ID)
+        {
+            return countries.GetAll().Where(t => t.ID == ID).FirstOrDefault();
+        }
+
+        public Country GetCountryByName(string Name)
+        {
+            return countries.GetAll().Where(t => t.Name.ToLower() == Name.ToLower()).FirstOrDefault();
+        }
+
+        public bool IsUpdateAllowed(int ID, string Name)
+        {
+            if (countries.GetAll().Where(t => t.Name == Name).Where(t => t.ID != ID).FirstOrDefault() != null)
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        public bool AddNewCountry(Country country)
+        {
+            return countries.Add(country);
+        }
+
+        public bool UpdateCountry(Country country)
+        {
+            return countries.Update(country);
+        }
+
+        public bool DeleteCountry(Country country)
+        {
+            return countries.Delete(country);
+        }
+
+        public bool DeleteCountryByID(int ID)
+        {
+            return countries.Delete(new Country() { ID = ID });
+        }
+    }
+}
